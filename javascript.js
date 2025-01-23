ee5a28e94f86fedafe89e5c1294cb8d98209b918
Fabian Herrera <fabianherrerap73@gmail.com>
2025-01-23 11:04:04 -0500

    Finish grid, add hover effect for drawing effect.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,23 @@
-// Create 256 (16x16) divs
+// Create variable for the container of all the divs forming the div
+const container = document.querySelector(".container");
+
+// Create 256 (16x16) divs (grid)
+for(let i = 0 ; i < 256 ; i ++){
+    const div = document.createElement("div");
+    div.classList.add("grid");
+    div.style.height = "40px";
+    div.style.width = "40px";
+    div.style.border = "1px solid black";
+    container.appendChild(div);
+}
+
+// Select all the boxes of the grid
+const grids = document.querySelectorAll(".grid");
+grids.forEach(grid =>{
+    grid.addEventListener("mouseenter", ()=>{
+        grid.style.backgroundColor = "blue";
+    });
+    grid.addEventListener("mouseleave", ()=>{
+        grid.style.backgroundColor = "black";
+    });
+});
